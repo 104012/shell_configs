f8c1a2bbd677c47261dc076f517c20f3e05f174e
popo <pppopo1@hotmail.com>
2015-05-16 16:02:47 +0200

    stupid quotes

--- deploy.sh
+++ deploy.sh
@@ -15,17 +15,17 @@ function vimrc() {
     local src="vimrc"
     local dest="$HOME/.vimrc"
 
-    cp -i "$src" "$dest"
+    cp -i $src $dest
 }
 
 function bashrc() {
     local src="bashrc"
     local dest="$HOME/.bashrc"
 
-    if [[ -e "$dest" ]]; then
-        cat "$src" >> "$dest"
+    if [[ -e $dest ]]; then
+        cat $src >> $dest
     else
-        cp -i "$src" "$dest"
+        cp -i $src $dest
     fi
 }
 
